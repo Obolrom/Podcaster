7c089fae770dbe37e57ea3aab034d60e4234cfae
Obolrom <obolrom@gmail.com>
2021-09-05 17:45:36 +0300

    error handling on api call

--- app/src/main/java/io/obolonsky/podcaster/viewmodels/SongsViewModel.kt
+++ app/src/main/java/io/obolonsky/podcaster/viewmodels/SongsViewModel.kt
@@ -6,8 +6,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import io.obolonsky.podcaster.data.repositories.SongsRepository
 import io.obolonsky.podcaster.data.responses.MusicItem
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 class SongsViewModel @Inject constructor(
@@ -20,6 +22,7 @@ class SongsViewModel @Inject constructor(
     fun loadSongs() {
         viewModelScope.launch {
             songsRepository.getMusicItems()
+                .catch { Timber.e(it) }
                 .collect { songs ->
                     _songs.value = songs
                 }

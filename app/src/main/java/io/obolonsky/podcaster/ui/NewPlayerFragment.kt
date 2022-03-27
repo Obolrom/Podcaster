f7dff25bc419c9ee612ec0e0ddcb41cdfa3fcbe7
Obolrom <obolrom@gmail.com>
2022-03-27 15:42:20 +0300

    player init

---
+++ app/src/main/java/io/obolonsky/podcaster/ui/NewPlayerFragment.kt
@@ -0,0 +1,11 @@
+package io.obolonsky.podcaster.ui
+
+import android.os.Bundle
+import io.obolonsky.podcaster.R
+
+class NewPlayerFragment : AbsFragment(R.layout.fragment_new_player) {
+
+    override fun initViewModels() { }
+
+    override fun initViews(savedInstanceState: Bundle?) { }
+}

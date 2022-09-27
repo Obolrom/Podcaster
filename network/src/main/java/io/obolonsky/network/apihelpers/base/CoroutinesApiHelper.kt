6f0fad4e5ac6b504da1568e48f32fc7d7b26bda5
Obolrom <obolrom@gmail.com>
2022-09-27 22:42:46 +0300

    Refactored base coroutines api helper

--- network/src/main/java/io/obolonsky/network/apihelpers/base/CoroutinesApiHelper.kt
+++ network/src/main/java/io/obolonsky/network/apihelpers/base/CoroutinesApiHelper.kt
@@ -6,7 +6,7 @@ import io.obolonsky.core.di.Reaction
 import io.obolonsky.core.di.utils.CoroutineSchedulers
 import io.obolonsky.core.di.utils.Mapper
 import io.obolonsky.network.utils.runWithReaction
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.flow.*
 
 abstract class CoroutinesApiHelper<ApiResult, DomainResult, ApiParam>(
     private val dispatchers: CoroutineSchedulers,
@@ -14,18 +14,16 @@ abstract class CoroutinesApiHelper<ApiResult, DomainResult, ApiParam>(
 ) : ApiHelper<DomainResult, ApiParam> {
 
     /**
-     * Already executes on [CoroutineSchedulers.computation]
+     * Already executes on [CoroutineSchedulers.io]
      */
     abstract suspend fun apiRequest(param: ApiParam): NetworkResponse<ApiResult, *>
 
     final override suspend fun load(param: ApiParam): Reaction<DomainResult, Error> {
-        val apiResult = withContext(dispatchers.io) {
-            apiRequest(param)
-        }
-        return apiResult.runWithReaction {
-            withContext(dispatchers.computation) {
-                mapper.map(this@runWithReaction)
-            }
-        }
+        return flowOf(apiRequest(param))
+            .flowOn(dispatchers.io)
+            .map { it.runWithReaction { mapper.map(this) } }
+            .flowOn(dispatchers.computation)
+            .catch { emit(Reaction.Fail(Error.UnknownError(it))) }
+            .single()
     }
 }

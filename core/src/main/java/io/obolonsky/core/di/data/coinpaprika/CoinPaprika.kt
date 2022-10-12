725e1d1ba8f19e9f72b404a9591d235ea4613cf5
Obolrom <obolrom@gmail.com>
2022-10-12 23:43:20 +0300

    Coin paprika details with logo cross fade loading

--- core/src/main/java/io/obolonsky/core/di/data/coinpaprika/CoinPaprika.kt
+++ core/src/main/java/io/obolonsky/core/di/data/coinpaprika/CoinPaprika.kt
@@ -5,7 +5,17 @@ data class CoinPaprika(
     val isActive: Boolean,
     val isNew: Boolean?,
     val name: String,
+    val logo: String?,
     val rank: Int,
     val symbol: String?,
-    val type: String?
-)
+    val type: String?,
+    val tags: List<Tag>,
+) {
+
+    data class Tag(
+        val id: String,
+        val name: String,
+        val coinCounter: Int,
+        val icoCounter: Int?,
+    )
+}

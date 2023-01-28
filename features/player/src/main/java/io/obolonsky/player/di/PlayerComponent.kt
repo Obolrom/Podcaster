e425103079f5dc670308483fd885bb75f43cd2e4
Obolrom <obolrom@gmail.com>
2023-01-28 21:36:51 +0200

    Fixed component dependencies

--- features/player/src/main/java/io/obolonsky/player/di/PlayerComponent.kt
+++ features/player/src/main/java/io/obolonsky/player/di/PlayerComponent.kt
@@ -1,9 +1,12 @@
 package io.obolonsky.player.di
 
 import dagger.Component
-import io.obolonsky.core.di.depsproviders.ApplicationProvider
+import io.obolonsky.core.di.depsproviders.ToolsProvider
+import io.obolonsky.core.di.downloads.providers.DownloaderProvider
 import io.obolonsky.core.di.downloads.providers.GetDownloadServiceClassActionProvider
 import io.obolonsky.core.di.downloads.providers.StartDownloadServiceActionProvider
+import io.obolonsky.core.di.player.PlayerDependenciesProvider
+import io.obolonsky.core.di.repositories.providers.ShazamRepoProvider
 import io.obolonsky.core.di.scopes.FeatureScope
 import io.obolonsky.player.PlayerFragment
 import io.obolonsky.player.player.PodcasterPlaybackService
@@ -12,7 +15,10 @@ import io.obolonsky.player.viewmodels.DownloadViewModel
 @FeatureScope
 @Component(
     dependencies = [
-        ApplicationProvider::class,
+        ToolsProvider::class,
+        ShazamRepoProvider::class,
+        PlayerDependenciesProvider::class,
+        DownloaderProvider::class,
         StartDownloadServiceActionProvider::class,
         GetDownloadServiceClassActionProvider::class,
     ],
@@ -26,7 +32,10 @@ internal interface PlayerComponent {
     interface Factory {
 
         fun create(
-            applicationProvider: ApplicationProvider,
+            toolsProvider: ToolsProvider,
+            shazamRepoProvider: ShazamRepoProvider,
+            playerDependenciesProvider: PlayerDependenciesProvider,
+            downloaderProvider: DownloaderProvider,
             startDownloadServiceActionProvider: StartDownloadServiceActionProvider,
             getDownloadServiceClassActionProvider: GetDownloadServiceClassActionProvider,
         ): PlayerComponent

f2119126ab9c1b9ba2d91adc392eacc42203e6f5
Obolrom <obolrom@gmail.com>
2023-03-04 22:31:26 +0200

    Fixed lifecycle

--- features/github/src/main/java/io/obolonsky/github/ui/SearchReposFragment.kt
+++ features/github/src/main/java/io/obolonsky/github/ui/SearchReposFragment.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.widget.doOnTextChanged
@@ -49,6 +50,9 @@ class SearchReposFragment : Fragment(R.layout.fragment_search_repos) {
             viewModel.repoNameSearchQuery.value = text.toString()
         }
 
+        binding.searchResultContainer.setViewCompositionStrategy(
+            ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+        )
         binding.searchResultContainer.setContent {
             val state by viewModel.collectAsState()
 

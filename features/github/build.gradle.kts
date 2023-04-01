82635c294516856e73261529a579e26c73196b94
Obolrom <obolrom@gmail.com>
2023-04-01 19:28:48 +0300

    Updated compose to latest version

--- features/github/build.gradle.kts
+++ features/github/build.gradle.kts
@@ -71,14 +71,14 @@ dependencies {
     // Compose
     val composeBom = platform("androidx.compose:compose-bom:2023.01.00")
     implementation(composeBom)
-    implementation("androidx.compose.ui:ui:1.3.3")
-    implementation("androidx.compose.material:material:1.3.1")
-    implementation("androidx.compose.material:material-icons-core:1.3.1")
-    implementation("androidx.compose.material:material-icons-extended:1.3.1")
-    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("androidx.compose.ui:ui:1.4.0")
+    implementation("androidx.compose.material:material:1.4.0")
+    implementation("androidx.compose.material:material-icons-core:1.4.0")
+    implementation("androidx.compose.material:material-icons-extended:1.4.0")
+    implementation("androidx.activity:activity-compose:1.7.0")
     // Android Studio Preview support
-    implementation("androidx.compose.ui:ui-tooling-preview:1.3.3")
-    implementation("androidx.compose.ui:ui-tooling:1.3.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.0")
+    implementation("androidx.compose.ui:ui-tooling:1.4.0")
 
     // Network
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
